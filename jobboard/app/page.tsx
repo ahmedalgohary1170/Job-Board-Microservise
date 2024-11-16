f5d039d4cfa79ae2bd96cb288d512166bdbd970f
ahmed_algohary <ahmedalgohary1170@gmail.com>
2024-11-16 13:20:50 +0200

    add shadcn button

--- jobboard/app/page.tsx
+++ jobboard/app/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button"
+
 
 export default function Home() {
   return (
@@ -12,6 +14,7 @@ export default function Home() {
           height={38}
           priority
         />
+        <Button>Click me</Button>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
