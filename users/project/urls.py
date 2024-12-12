109bd4f2e1e027a3630793e17c8a860811d8dd69
ahmedalgohary1170 <ahmedalgohary1170@gmail.com>
2024-12-12 16:31:27 +0200

    add accounts urls , add swagger docs in blog , users

--- users/project/urls.py
+++ users/project/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 
 
 from django.conf import settings
@@ -27,9 +27,9 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Jobs API",
+      title="Users API",
       default_version='v1',
-      description="JobBoard app API",
+      description="Users App API",
       contact=openapi.Contact(email="ahmedalgohary1170@gmail.com"),
       license=openapi.License(name="Ahmed Algohary"),
    ),
@@ -39,6 +39,12 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/',include('accounts.urls')),
+
+
+
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
 
